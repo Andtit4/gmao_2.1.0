32d88c701963ff9c751f2a86b3b235b92cf92399
Andtit4 <=>
2023-05-20 08:55:02 +0100

    init vuesax

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from  'bootstrap-vue' 
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css'
 
 // Importation des fichiers CSS Bootstrap et BootstrapVue (l'ordre est important) 
 import  'bootstrap/dist/css/bootstrap.css' 
@@ -9,7 +12,7 @@ import  'bootstrap-vue/dist/bootstrap-vue.css'
 // Rendre BootstrapVue disponible dans tout votre projet 
 Vue.use(BootstrapVue) // Installer éventuellement le plugin de composants d'icône BootstrapVue 
 Vue.use(IconsPlugin)
-
+Vue.use(Vuesax)
 Vue.config.productionTip = false
 
 new Vue({

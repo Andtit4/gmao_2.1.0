e13fcb3675ace7020e4527354c7b3d68e280ca9e
Andtit4 <lirsitogo2021@gmail.com>
2024-01-09 08:57:43 +0000

    init login at the start

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 // import Style from '@/views/StyleView.vue'
 import Home from '@/views/HomeView.vue'
-import LoginViewVue from '@/views/LoginView.vue'
+import Login from '@/views/LoginView.vue'
 
 const routes = [
-  /* {
+ /*  {
     meta: {
       title: 'Select style'
     },
     path: '/',
     name: 'style',
-    component: Style
+    component: Login
   }, */
   {
     meta: {
       title: 'Login'
     },
     path: '/',
-    name: 'login',
-    component: LoginViewVue
+    name: 'style',
+    component: Login
   },
   {
     // Document title tag

a79bf6bf05f73b3da4be590081e3f59c6efa9a8e
Andtit4 <lirsitogo2021@gmail.com>
2024-01-26 16:06:27 +0000

    modified:   src/views/sites/siteList.vue

--- src/views/sites/siteList.vue
+++ src/views/sites/siteList.vue
@@ -432,45 +432,47 @@ onMounted(() => {
   <FormField label="Rechercher">
     <FormControl v-model="form.searchSite" placeholder="Entrez le nom du site" @input="search()" />
     <FormControl v-model="form.searchZone" placeholder="Entrez la zone" @input="searchZone()" />
-
   </FormField>
   <br />
-  <table>
-    <thead>
-      <tr>
-        <th v-if="checkable" />
-        <th />
-        <th>Site Id</th>
-        <th>Nom Site</th>
-        <th>Zone</th>
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(site, index) in sites.list" :key="index">
-        <TableCheckboxCell v-if="checkable" @checked="checked($event, site)" />
-        <td class="border-b-0 lg:w-6 before:hidden">
-          <!-- <UserAvatar :username="site.nom_site" class="w-24 h-24 mx-auto lg:w-6 lg:h-6" /> -->
-        </td>
-        <td data-label="Site Id">
-          {{ site.site_id }}
-        </td>
-        <td data-label="Nom site">
-          {{ site.nom_site }}
-        </td>
-        <td data-label="Zone">
-          {{ site.zone }}
-        </td>
-        <td class="before:hidden lg:w-1 whitespace-nowrap">
-          <BaseButtons type="justify-start lg:justify-end" no-wrap>
-            <BaseButton color="success" :icon="mdiPencil" small @click="editSite(site._id)" />
-            <BaseButton color="info" :icon="mdiEye" small @click="showSite(site._id)" />
-            <BaseButton color="danger" :icon="mdiTrashCan" small @click="deleteSite(site._id)" />
-          </BaseButtons>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="max-h-[32rem] overflow-x-auto">
+    <table>
+      <thead>
+        <tr>
+          <th v-if="checkable" />
+          <th />
+          <th>Site Id</th>
+          <th>Nom Site</th>
+          <th>Zone</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(site, index) in sites.list" :key="index">
+          <TableCheckboxCell v-if="checkable" @checked="checked($event, site)" />
+          <td class="border-b-0 lg:w-6 before:hidden">
+            <!-- <UserAvatar :username="site.nom_site" class="w-24 h-24 mx-auto lg:w-6 lg:h-6" /> -->
+          </td>
+          <td data-label="Site Id">
+            {{ site.site_id }}
+          </td>
+          <td data-label="Nom site">
+            {{ site.nom_site }}
+          </td>
+          <td data-label="Zone">
+            {{ site.zone }}
+          </td>
+          <td class="before:hidden lg:w-1 whitespace-nowrap">
+            <BaseButtons type="justify-start lg:justify-end" no-wrap>
+              <BaseButton color="success" :icon="mdiPencil" small @click="editSite(site._id)" />
+              <BaseButton color="info" :icon="mdiEye" small @click="showSite(site._id)" />
+              <BaseButton color="danger" :icon="mdiTrashCan" small @click="deleteSite(site._id)" />
+            </BaseButtons>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
   <div class="p-3 lg:px-6 border-t border-gray-100 dark:border-slate-800">
     <BaseLevel>
       <BaseButtons>

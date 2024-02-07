81d526135fac9aaa8be645f0c5ca0048e6435a29
Andtit4 <lirsitogo2021@gmail.com>
2024-02-07 01:18:52 +0000

    add waiting marker for the map

--- src/layouts/TogoMapComponent.vue
+++ src/layouts/TogoMapComponent.vue
@@ -14,7 +14,7 @@ const togoCoordinates = reactive({
 const redMarkerIcon = new L.Icon({
   iconUrl:
     'https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/map_marker_base-512.png',
-  iconSize: [20, 20],
+  iconSize: [10, 10],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
@@ -25,7 +25,17 @@ const redMarkerIcon = new L.Icon({
 const greenMarkerIcon = new L.Icon({
   iconUrl:
     'https://play-lh.googleusercontent.com/5WifOWRs00-sCNxCvFNJ22d4xg_NQkAODjmOKuCQqe57SjmDw8S6VOSLkqo6fs4zqis',
-  iconSize: [20, 20],
+  iconSize: [10, 10],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+})
+
+const warningMarkerIcon = new L.Icon({
+  iconUrl:
+    'https://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-Free-Download-PNG.png',
+  iconSize: [10, 10],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
@@ -34,6 +44,7 @@ const greenMarkerIcon = new L.Icon({
 
 const cardSiteList = reactive({ list: [] })
 const cardSiteDoneList = reactive({ list: [] })
+const cardWaitingList = reactive({ list: [] })
 
 const siteNonFait = async () => {
   axios({
@@ -50,7 +61,14 @@ const siteNonFait = async () => {
       })
         .then((r) => {
           cardSiteDoneList.list = r.data
-          createCard()
+
+          axios({
+            url: apiService.getUrl() + '/plannifie/encours/for-map',
+            method: 'GET'
+          }).then((r) => {
+            cardWaitingList.list = r.data
+            createCard()
+          })
         })
         .catch((err) => {
           console.log("Error lors de l'affichage des site fait sur la carte", err.message)
@@ -64,7 +82,7 @@ const siteNonFait = async () => {
 const createCard = () => {
   const map = L.map('map', {
     center: [togoCoordinates.latitude, togoCoordinates.longitude],
-    zoom: 7
+    zoom: 8
   }) /* .setView(togoCoordinates.latitude, togoCoordinates.longitude, 8) */
 
   // Ajout d'une couche de carte OpenStreetMap
@@ -87,23 +105,11 @@ const createCard = () => {
     marker.bindPopup(data.nom_site) // Utiliser la zone ou d'autres informations pertinentes pour la popup
   })
 
-  /* for (let data = 0; data <= cardSiteList.list.length; data++) {
-    console.log('mapping : ', cardSiteList.list[data])
-    L.marker([cardSiteList.list[data].latitude, cardSiteList.list[data].longitude], {
-      icon: redMarkerIcon
-    })
-      .bindPopup(cardSiteList.list[data].zone)
-      .addTo(map)
-  } */
+  cardWaitingList.list.forEach((data) => {
+    const marker = L.marker([data.latitude, data.longitude], { icon: warningMarkerIcon }).addTo(map)
+    marker.bindPopup(data.nom_site) // Utiliser la zone ou d'autres informations pertinentes pour la popup
+  })
 
-  /* for (let data = 0; data <= cardSiteDoneList.list.length; data++) {
-    console.log('mapping done : ', cardSiteDoneList.list[data])
-    L.marker([cardSiteDoneList.list[data].latitude, cardSiteDoneList.list[data].longitude], {
-      icon: greenMarkerIcon
-    })
-      .bindPopup(cardSiteDoneList.list[data].zone)
-      .addTo(map)
-  } */
   // Limiter la carte aux frontières du Togo
   map.setMaxBounds(bounds)
   map.on('drag', () => {

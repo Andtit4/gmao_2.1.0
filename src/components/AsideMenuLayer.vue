fe7c7d47de380eada40c40b2c69ba0a027e55f33
Andtit4 <lirsitogo2021@gmail.com>
2024-12-14 10:23:40 +0000

    change logo as build

--- src/components/AsideMenuLayer.vue
+++ src/components/AsideMenuLayer.vue
@@ -38,7 +38,7 @@ const asideLgCloseClick = (event) => {
     <div class="aside lg:rounded-2xl flex-1 flex flex-col overflow-hidden dark:bg-slate-900">
       <div class="aside-brand flex flex-row h-14 items-center justify-between dark:bg-slate-900">
         <div class="text-center flex-1 lg:text-left lg:pl-6 xl:text-center xl:pl-0">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Yas_Tanzania.svg/1200px-Yas_Tanzania.svg.png" alt="" width="30" style="position: relative; top: 30px; left: 15px">
+          <img src="@/assets/logo.png" alt="" width="30" style="position: relative; top: 30px; left: 15px">
           <b class="font-black">Energy Eye</b>
         </div>
         <button class="hidden lg:inline-block xl:hidden p-3" @click.prevent="asideLgCloseClick">
